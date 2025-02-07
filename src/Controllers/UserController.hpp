2472efe89556593ca84e6b3cafe8fdc20a6b0258
=Softadastra <=softadastra@gmail.com>
2025-02-07 16:55:57 +0300

    creation d'une route updateUser() et ajout du frontend

--- src/Controllers/UserController.hpp
+++ src/Controllers/UserController.hpp
@@ -73,14 +73,12 @@ namespace Softadastra
 
                                              if (!users.empty())
                                              {
-                                                 res.result(http::status::ok);
-                                                 res.set(http::field::content_type, "application/json");
                                                  nlohmann::json users_json = nlohmann::json::array();
                                                  for (const auto &user : users)
                                                  {
                                                      users_json.push_back(user.to_json());
                                                  }
-                                                 res.body() = users_json.dump();
+                                                 Response::json_response(res, users_json);
                                              }
                                              else
                                              {
@@ -108,9 +106,6 @@ namespace Softadastra
 
                                              if (user)
                                              {
-                                                 //  res.result(http::status::ok);
-                                                 //  res.set(http::field::content_type, "application/json");
-                                                 //  res.body() = user->to_json().dump();
                                                  Softadastra::Response::json_response(res, user->to_json());
                                              }
                                              else
@@ -160,6 +155,54 @@ namespace Softadastra
                                          catch (const nlohmann::json::exception &e)
                                          {
                                              Response::error_response(res, http::status::bad_request, "Invalid JSON format");
+                                         }
+                                         catch (const std::exception &e)
+                                         {
+                                             Softadastra::Response::error_response(res, http::status::internal_server_error, e.what());
+                                         }
+                                     })));
+
+            router.add_route(http::verb::put, "/update/{id}",
+                             std::static_pointer_cast<IRequestHandler>(
+                                 std::make_shared<DynamicRequestHandler>(
+                                     [self](const std::unordered_map<std::string, std::string> &params,
+                                            http::response<http::string_body> &res)
+                                     {
+                                         try
+                                         {
+                                             // Récupérer l'ID de l'utilisateur à partir de l'URL
+                                             std::stringstream ss(params.at("id"));
+                                             int id{};
+                                             ss >> id;
+
+                                             // Vérifier que le corps de la requête contient les champs nécessaires
+                                             json request_json;
+                                             try
+                                             {
+                                                 request_json = json::parse(params.at("body"));
+                                             }
+                                             catch (const std::exception &e)
+                                             {
+                                                 Response::error_response(res, http::status::bad_request, "Invalid JSON body");
+                                                 return;
+                                             }
+
+                                             if (request_json.find("firstname") == request_json.end())
+                                             {
+                                                 Response::error_response(res, http::status::bad_request, "Le champ 'firstname' est manquant.");
+                                                 return;
+                                             }
+                                             if (request_json.find("email") == request_json.end())
+                                             {
+                                                 Response::error_response(res, http::status::bad_request, "Le champ 'email' est manquant.");
+                                                 return;
+                                             }
+
+                                             // Appeler la fonction updateUser pour mettre à jour l'utilisateur dans la base de données
+                                             User updated_user = self->updateUser(id, request_json["firstname"], request_json["email"]);
+
+                                             // Répondre avec un message de succès et les informations de l'utilisateur mis à jour
+                                             Softadastra::Response::json_response(res, updated_user.to_json());
                                          }
                                          catch (const std::exception &e)
                                          {
@@ -221,6 +264,53 @@ namespace Softadastra
             }
         }
 
+        User updateUser(int user_id, const std::string &full_name, const std::string &email)
+        {
+            try
+            {
+                // Se connecter à la base de données
+                std::unique_ptr<sql::Connection> con = getDbConnection();
+                if (!con)
+                {
+                    throw std::runtime_error("La connexion à la base de données a échoué.");
+                }
+
+                // Préparer la requête SQL UPDATE
+                std::unique_ptr<sql::PreparedStatement> pstmt(
+                    con->prepareStatement("UPDATE test_user SET full_name = ?, email = ? WHERE id = ?"));
+
+                // Lier les paramètres (full_name, email et user_id)
+                pstmt->setString(1, full_name);
+                pstmt->setString(2, email);
+                pstmt->setInt(3, user_id);
+
+                // Exécuter la requête UPDATE
+                int rows_affected = pstmt->executeUpdate();
+
+                // Si aucune ligne n'a été affectée, cela signifie que l'utilisateur n'existe pas
+                if (rows_affected == 0)
+                {
+                    throw std::runtime_error("Aucun utilisateur trouvé avec cet ID.");
+                }
+
+                // Récupérer les informations mises à jour
+                User updated_user;
+                updated_user.setId(user_id);
+                updated_user.setFullName(full_name);
+                updated_user.setEmail(email);
+
+                return updated_user;
+            }
+            catch (const sql::SQLException &e)
+            {
+                throw std::runtime_error("Erreur lors de la mise à jour de l'utilisateur : " + std::string(e.what()));
+            }
+            catch (const std::exception &e)
+            {
+                throw std::runtime_error("Erreur lors de la mise à jour de l'utilisateur : " + std::string(e.what()));
+            }
+        }
+
         std::optional<User> get_user_by_id(int userId)
         {
             try
@@ -231,7 +321,7 @@ namespace Softadastra
                     throw std::runtime_error("La connexion à la base de données a échoué.");
                 }
 
-                std::unique_ptr<sql::PreparedStatement> pstmt(con->prepareStatement("SELECT * FROM tbl_user WHERE id = ?"));
+                std::unique_ptr<sql::PreparedStatement> pstmt(con->prepareStatement("SELECT * FROM test_user WHERE id = ?"));
                 pstmt->setInt(1, userId);
                 std::shared_ptr<sql::ResultSet> res(pstmt->executeQuery());
 
@@ -267,7 +357,7 @@ namespace Softadastra
                     throw std::runtime_error("La connexion à la base de données a échoué.");
                 }
 
-                std::unique_ptr<sql::PreparedStatement> pstmt(con->prepareStatement("SELECT * FROM tbl_user"));
+                std::unique_ptr<sql::PreparedStatement> pstmt(con->prepareStatement("SELECT * FROM test_user"));
                 std::shared_ptr<sql::ResultSet> res(pstmt->executeQuery());
 
                 while (res->next())
