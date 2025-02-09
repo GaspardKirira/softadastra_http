4edb47cdd89f1ef630c560d47991675727ec20f4
=Softadastra <=softadastra@gmail.com>
2025-02-10 00:53:57 +0300

    Ajout d'une route /predict qui communique avec python

--- src/Controllers/HomeController.hpp
+++ src/Controllers/HomeController.hpp
@@ -8,6 +8,7 @@
 #include <nlohmann/json.hpp>
 #include <ctime>
 #include <unordered_set>
+#include <sstream>
 #include "Controller.hpp"
 
 namespace Softadastra
@@ -20,14 +21,61 @@ namespace Softadastra
         void configure(Router &routes) override
         {
             routes.add_route(
-                http::verb::get, "/",
+                http::verb::post, "/predict",
                 std::static_pointer_cast<IRequestHandler>(
                     std::make_shared<DynamicRequestHandler>(
-                        [](const std::unordered_map<std::string, std::string> &,
-                           http::response<http::string_body> &res)
+                        [this](const std::unordered_map<std::string, std::string> &params,
+                               http::response<http::string_body> &res)
                         {
-                            Response::success_response(res, "Welcome to the Softadastra HTTP Server");
+                            handler_predict(params, res);
                         })));
+        }
+        void handler_predict(const std::unordered_map<std::string, std::string> &params, http::response<http::string_body> &res)
+        {
+            try
+            {
+
+                json json_data;
+                try
+                {
+                    json_data = json::parse(params.at("body"));
+                }
+                catch (const std::exception &e)
+                {
+                    Response::error_response(res, http::status::bad_request, "Invalid JSON");
+                    return;
+                }
+
+                if (json_data.find("revenu") == json_data.end())
+                {
+                    Response::error_response(res, http::status::bad_request, "Le champ 'revenu' est manquant");
+                    return;
+                }
+
+                double revenu = json_data["revenu"];
+                double predicted_cost = predict(revenu);
+
+                json response_json;
+                response_json["revenu"] = revenu;
+                response_json["predicted_cost"] = predicted_cost;
+
+                Response::json_response(res, response_json);
+            }
+            catch (const json::exception &e)
+            {
+                std::cout << "JSON exception: " << e.what() << std::endl;
+                Response::error_response(res, http::status::bad_request, "Invalid JSON format");
+            }
+            catch (const std::exception &e)
+            {
+                std::cout << "General exception: " << e.what() << std::endl;
+                Response::error_response(res, http::status::internal_server_error, e.what());
+            }
+        }
+
+        inline double predict(double revenu)
+        {
+            return revenu * 0.3;
         }
     };
 } // namespace Softadastra
