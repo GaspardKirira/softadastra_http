9e4ed3bc72479ef49d15e1f099c56ffd03a519d4
=Softadastra <=softadastra@gmail.com>
2025-02-23 21:59:48 +0300

    le serveur accepte plusieurs connexion apres une amelioration de la classe Session

--- src/core/http/HTTPServer.hpp
+++ src/core/http/HTTPServer.hpp
@@ -37,70 +37,24 @@ namespace Softadastra
     using json = nlohmann::json;
 
     constexpr size_t NUMBER_OF_THREADS = 8;
-
-    /**
-     * @class HTTPServer
-     * @brief Represents an HTTP server that listens for incoming client connections.
-     *
-     * The HTTPServer class is responsible for initializing the server, accepting client
-     * connections, and delegating requests to appropriate handlers. It uses a thread pool
-     * for handling incoming requests asynchronously.
-     */
     class HTTPServer
     {
     public:
-        /**
-         * @brief Constructor to initialize the HTTP server with a configuration object.
-         *
-         * @param config The configuration object that contains server settings (e.g., port, routes).
-         */
         explicit HTTPServer(Config &config);
         ~HTTPServer();
-
-        // void handle_signals();
-
-        // void stop();
-
-        /**
-         * @brief Starts the server and begins accepting incoming connections.
-         *
-         * This method initializes the I/O context, starts the acceptor, and begins listening
-         * for incoming client connections. Once a connection is accepted, it is handled by
-         * the `handle_client` method.
-         */
         void run();
-
-        /**
-         * @brief Starts the acceptor to listen for incoming client connections.
-         *
-         * This method begins the asynchronous accept operation, where new client connections
-         * will be accepted and passed to the `handle_client` method.
-         */
         void start_accept();
 
     private:
-        /**
-         * @brief Handles a client connection and processes the request.
-         *
-         * This method is responsible for managing the client connection, which includes creating
-         * a session for the client, routing the request, and sending back the response.
-         *
-         * @param socket_ptr A shared pointer to the socket used for communication with the client.
-         * @param router A reference to the Router used to handle the HTTP request.
-         */
         void handle_client(std::shared_ptr<tcp::socket> socket_ptr, Router &router);
-
         void close_socket(std::shared_ptr<tcp::socket> socket);
-
         Config &config_;
         std::shared_ptr<net::io_context> io_context_;
         std::unique_ptr<tcp::acceptor> acceptor_;
         Router router_;
         std::unique_ptr<RouteConfigurator> route_configurator_;
-
         Softadastra::ThreadPool request_thread_pool_;
         std::vector<std::thread> io_threads_;
-
         std::atomic<bool> stop_requested_;
     };
 
